7d4534465f79b0949be1801bb9c01996d7d77fc8
Aleksey Zakharov <flex021981@gmail.com>
2020-09-28 11:02:03 +0300

    2. Сокобан. Тестовое задание. [#369250] - Исправил ошибки

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -83,25 +83,4 @@ public class Logic {
         return Arrays.toString(convert());
     }
 
-    public static boolean monoVertical(int[][] board, int column) {
-        boolean result = true;
-        for (int i = 0; i < board[column].length; i++) {
-            if (board[i][column] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static boolean monoHorizontal(int[][] board, int row) {
-        boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
 }
--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,11 +4,33 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (Logic.monoHorizontal(board, i) || Logic.monoVertical(board, i)) {
+            if (board[i][i] == 1 && monoHorizontal(board, i) || monoVertical(board, i)) {
                 rsl = true;
                 break;
             }
         }
         return rsl;
+    }
+
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board[column].length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
